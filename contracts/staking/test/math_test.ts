7e345cb81122b4a8a3e6c120b4a0560275e8a59f
Michael Zhu <mzhu25@stanford.edu>
2019-08-27 11:41:39 -0700

    update tests to use new StakingWrapper constructor definition

--- contracts/staking/test/math_test.ts
+++ contracts/staking/test/math_test.ts
@@ -41,7 +41,7 @@ describe('Math Libraries', () => {
         [zrxTokenContract] = await erc20Wrapper.deployDummyTokensAsync(1, ZRX_TOKEN_DECIMALS);
         await erc20Wrapper.setBalancesAndAllowancesAsync();
         // deploy staking contracts
-        stakingWrapper = new StakingWrapper(provider, owner, erc20ProxyContract, zrxTokenContract, accounts);
+        stakingWrapper = new StakingWrapper(provider, owner, erc20ProxyContract, zrxTokenContract);
         await stakingWrapper.deployAndConfigureContractsAsync();
     });
     beforeEach(async () => {
